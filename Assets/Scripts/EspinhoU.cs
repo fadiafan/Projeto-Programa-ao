cf61cdd368204fabfd772ce8f24dd99305ab0fd8
Restart Alunos <Restart Alunos@DESKTOP-U6B5F6V>
2020-02-03 21:41:15 +0000

    Jogo Programaçao

---
+++ Assets/Scripts/EspinhoU.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EspinhoU : MonoBehaviour
+{
+    private Rigidbody2D espinhoRb;
+    private int atrito;
+    public int atritoMaximo, atritoMinimo;
+    public Vector3 posicao;
+    public GameObject espinhoPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        espinhoRb = GetComponent<Rigidbody2D>();
+        atrito = Random.Range(atritoMinimo, atritoMaximo);
+        espinhoRb.drag = atrito;
+
+        posicao = transform.position;
+    }
+
+    private void OnBecameInvisible()
+    {
+        Instantiate(espinhoPrefab, posicao, transform.localRotation);
+        Pontua.pontos += 1;
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D playerRb;
+    public float velocidade;
+    private SpriteRenderer playerSprite;
+    public bool flipx;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+        playerSprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            velocidade = velocidade * -1;
+            flipx = !flipx;
+            playerSprite.flipX = flipx;
+        }
+
+        playerRb.velocity = new Vector2(velocidade, 0);
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D colisao)
+    {
+        if(colisao.gameObject.tag == "Espinho")
+        {
+            SceneManager.LoadScene("gameover");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D colisao)
+    {
+        if (colisao.gameObject.tag == "Espinho")
+        {
+            GetComponent<Animator>().SetBool("Die", true);
+            //SceneManager.LoadScene("gameover");
+        }
+
+
+    }
+
+    public void GameOver()
+    {
+        SceneManager.LoadScene("gameover");
+    }
+}
